dfd00723fb2b4adcb84bee21c2605c76822241c6
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-05-31 02:18:23 +0300

    added c files

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *check_cycle - checks whether a singly linked list has a cycle in it
+ *@list:linked list to test
+ *Return:0 - no cycle, 1 - cycle exists
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *rabbit = list, *turtle = list;
+
+	while (rabbit && turtle && rabbit->next)
+	{
+		rabbit = rabbit->next->next;
+		turtle = turtle->next;
+		if (rabbit == turtle)
+			return (1);
+	}
+	return (0);
+}
