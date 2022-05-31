4c9b1430b57af1f1bf5afba8c2a7f6bd602cd5f8
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-05-31 21:00:17 +0300

    modified output

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+pre_num = "0"
 for i in range(100):
+    if i == 10:
+        pre_num = ""
     if i != 99:
-        print("{}".format(i), end=", ")
+        print("{}{}".format(pre_num, i), end=", ")
     else:
         print("{}".format(i))
