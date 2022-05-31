003fbcc7cde5ceab58fe5ba460e80a9bcf832cab
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-05-31 17:01:44 +0300

    second task

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from contextlib import nullcontext
+import random
+number = random.randint(-10000, 10000)
+last_digit = int(repr(number)[-1])
+str_val = None
+if last_digit > 5:
+    str_val = "and is greater than 5"
+elif last_digit == 0:
+    str_val = "and is 0"
+else:
+    str_val = "and is less than 6 and not 0"
+print(f"Last digit of {number} is {last_digit} {str_val}")
