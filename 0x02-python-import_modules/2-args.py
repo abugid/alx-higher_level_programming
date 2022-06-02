1ab512063f5680be68510d06afce643a086e2e20
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-06-02 12:28:31 +0300

    changed print format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ argv = sys.argv
 
 def main():
     str_end = "." if len(argv) == 1 else ":"
-    print(f"{len(argv) - 1} arguments{str_end}")
+    print("{} arguments{}".format(len(argv) - 1, str_end))
     for i in range(1, len(argv)):
-        print(f"{i} : {argv[i]}")
+        print("{} : {}".format(i, argv[i]))
 
 
 if __name__ == "__main__":
