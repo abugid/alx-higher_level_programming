0ab233989416b4bf3de2c060e093a1d1eafc409e
Biniyam Assefa <biniyam.rsa@gmail.com>
2022-06-02 12:40:47 +0300

    edited output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,8 @@ argv = sys.argv
 
 def main():
     str_end = "." if len(argv) == 1 else ":"
-    print("{} arguments{}".format(len(argv) - 1, str_end))
+    str_mid = "" if len(argv) == 2 else "s"
+    print("{} argument{}{}".format(len(argv) - 1, str_mid, str_end))
     for i in range(1, len(argv)):
         print("{} : {}".format(i, argv[i]))
 
